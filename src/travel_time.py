6f72431e2267aaa5cce96911c853a8b68021db97
G P Sanjay <gps.sanjay9@gmail.com>
2026-07-27 02:14:31 +0000

    c

--- src/travel_time.py
+++ src/travel_time.py
@@ -3,49 +3,22 @@ from pyspark.sql.window import Window
 
 
 def calculate_travel_times(arrival_df):
-    """
-    Calculate travel time between consecutive observed stops.
 
-    If multiple stop sequences are skipped, the total travel
-    time is divided equally among the missing links.
-
-    Output:
-        trip_id
-        route_id
-        from_sequence
-        to_sequence
-        link_travel_time
-    """
-
-    window = (
-        Window
-        .partitionBy("trip_id")
-        .orderBy("sequence")
-    )
+    window = Window.partitionBy("trip_id").orderBy("sequence")
 
     df = (
         arrival_df
-        .withColumn(
-            "next_sequence",
-            F.lead("sequence").over(window)
-        )
+        .withColumn("next_sequence", F.lead("sequence").over(window))
+        .withColumn("next_arrival", F.lead("arrival_time").over(window))
         .withColumn(
             "total_time",
-            F.unix_timestamp("next_arrival")
-            - F.unix_timestamp("arrival_time")
+            F.unix_timestamp("next_arrival") -
+            F.unix_timestamp("arrival_time")
         )
-    )
-
-    df = (
-        df
         .filter(F.col("next_sequence").isNotNull())
         .withColumn(
             "sequence_gap",
             F.col("next_sequence") - F.col("sequence")
-        )
-        .withColumn(
-            "total_time",
-            F.col("next_arrival") - F.col("arrival_time")
         )
         .filter(F.col("total_time") >= 0)
         .withColumn(
